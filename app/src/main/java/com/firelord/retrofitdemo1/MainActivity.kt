b1aa0c65df3bfeb65c326042e7142fafe523a177
FireLord <firelord.xda@gmail.com>
2022-07-15 09:08:11 +0530

    setup basic get retrofit
    Signed-off-by: FireLord <firelord.xda@gmail.com>

--- app/src/main/java/com/firelord/retrofitdemo1/MainActivity.kt
+++ app/src/main/java/com/firelord/retrofitdemo1/MainActivity.kt
@@ -2,10 +2,41 @@ package com.firelord.retrofitdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.firelord.retrofitdemo1.databinding.ActivityMainBinding
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        val retService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+
+        val responseLiveData:LiveData<Response<Albums>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList!=null){
+                while (albumsList.hasNext()){
+                    val albumsItem = albumsList.next()
+                    val result = " "+"Album Title : ${albumsItem.title}"+"\n"+
+                                 " "+"Album id : ${albumsItem.id}"+"\n"+
+                                 " "+"User id : ${albumsItem.userId}"+"\n\n\n"
+                    binding.textView.append(result)
+                }
+            }
+        })
     }
 }
